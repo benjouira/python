79da2d1f7cc468d42ca9259179ab3e9fa5b32e89
Rabii <rabi3jouira@gmail.com>
2022-06-18 16:45:01 +0100

    Add files via upload

---
+++ AllSchemas.py
@@ -0,0 +1,6 @@
+from flask_marshmallow import Marshmallow
+
+ma= Marshmallow()
+class JobSchema(ma.Schema):
+    class Meta:
+        fields = ("_class", "description", "displayName","name","url","builds")
